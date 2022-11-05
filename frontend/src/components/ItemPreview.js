cd94a5c203c774e67e68dced3230ae14d7d3ad4d
Rao Muhammad Akif Tufail <akifrao@gmail.com>
2022-11-05 23:37:47 +0500

    Update ItemPreview.js

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -3,7 +3,6 @@ import { Link } from "react-router-dom";
 import agent from "../agent";
 import { connect } from "react-redux";
 import { ITEM_FAVORITED, ITEM_UNFAVORITED } from "../constants/actionTypes";
-import placeholderImg from "../imgs/placeholder.png";
 
 const mapDispatchToProps = (dispatch) => ({
   favorite: (slug) =>
